9b3fc0ee193271243d0b4069308cc9c5c6f64b9e
Pavilion4ik <vladearth7@gmail.com>
2025-09-19 18:30:05 +0300

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,55 @@
 'use strict';
 
-// write code here
+const workerListItems = document.querySelectorAll('li');
+
+function getEmployees(listItems) {
+  const workers = [];
+
+  for (const listItem of listItems) {
+    workers.push({
+      name: listItem.textContent,
+      position: listItem.dataset.position,
+      salary: listItem.dataset.salary,
+      age: listItem.dataset.age,
+    });
+  }
+
+  return workers;
+}
+
+const workersObjects = getEmployees(workerListItems);
+
+function sortList(workersList) {
+  workersList.sort((a, b) => {
+    const workerSalaryFirst = Number(a.salary.replace(/[$,]/g, ''));
+    const workerSalarySecond = Number(b.salary.replace(/[$,]/g, ''));
+
+    if (workerSalaryFirst > workerSalarySecond) {
+      return -1;
+    }
+
+    if (workerSalaryFirst < workerSalarySecond) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  const documentList = document.querySelector('ul');
+
+  documentList.innerHTML = '';
+
+  workersList.forEach((worker) => {
+    const li = document.createElement('li');
+
+    li.textContent = worker.name;
+    li.dataset.position = worker.position;
+    li.dataset.salary = worker.salary;
+    li.dataset.age = worker.age;
+    documentList.appendChild(li);
+  });
+
+  return workersList;
+}
+
+sortList(workersObjects);
